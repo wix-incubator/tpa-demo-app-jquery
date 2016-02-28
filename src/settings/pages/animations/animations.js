395bce8009bf96d733d300de9344873662bd9c1c
Adiel <adiela@wix.com>
2016-02-28 11:02:54 +0200

    importing html partials with require text plugin

--- src/settings/pages/animations/animations.js
+++ src/settings/pages/animations/animations.js
@@ -1,4 +1,13 @@
 define(['$', 'text!./animations.html'], function ($, templateSrc) {
     var $tpl = $(templateSrc);
+
+    setTimeout(function () {
+        debugger;
+        var $playAnimationBtnCtrl = $tpl.find('#play-animation-btn').getCtrl();
+        $playAnimationBtnCtrl.onClick(function () {
+            console.log('Play Animation was clicked');
+        });
+    }, 200);
+
     return $tpl;
 });
