3b61ee3a60a541f94a9087507bca6f0b5d57364b
lotemh <lotemh@wix.com>
2016-03-30 15:51:50 +0300

    #SE-13993|clean dependencies + connect listeners

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -1,66 +1,34 @@
-define(['jquery', 'Wix', 'mustache'], function ($, Wix, mustache) {
-    return class Widget {
-        constructor(element) {
-            this.element = element;
-            this.settingsUpdate = {};
-            this.initialized = false;
-            this.template = null;
-        }
+(function() {
+    Wix.addEventListener(Wix.Events.SETTINGS_UPDATED, onSettingsUpdate);
 
-        init() {
-            this.updateCompHeight(600);
-            Wix.addEventListener(Wix.Events.SETTINGS_UPDATED, update => {
-                this.onSettingsUpdate(update);
-            });
+    // You can get the style params programmatically, un-comment the following snippet to see how it works:
+    /*Wix.Styles.getStyleParams(style => {
+     console.log(style);
+     });*/
 
-            // You can get the style params programmatically, un-comment the following snippet to see how it works:
-            /*Wix.Styles.getStyleParams(style => {
-                console.log(style);
-            });*/
+    // You can also get the style every time it changes, try this:
+    /*Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, style => {
+     console.log(style);
+     });*/
 
-            // You can also get the style every time it changes, try this:
-            /*Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, style => {
-                console.log(style);
-            });*/
+    function onSettingsUpdate(update) {
+        update = stringify(update);
+        $('.json').html(update);
+        updateCompHeight();
+    }
 
-            this.initialized = true;
-        }
+    function updateCompHeight(height) {
+        const desiredHeight = height || document.documentElement.scrollHeight;
+        Wix.setHeight(desiredHeight);
+    }
 
-        onSettingsUpdate(update) {
-            this.settingsUpdate = update;
-            this.render()
-                .then(() => this.updateCompHeight());
-        }
-
-        updateCompHeight(height) {
-            const desiredHeight = height || document.documentElement.scrollHeight;
-            Wix.setHeight(desiredHeight);
-        }
-
-        stringify(input) {
-            try {
-                return JSON.stringify(input, null, 4);
-            } catch (err) {
-                return input;
-            }
-        }
-
-        loadTemplate() {
-            if (this.template) {
-                return Promise.resolve(this.template);
-            } else {
-                return $.get('./widget.html').then(template => this.template = template);
-            }
-        }
-
-        render() {
-            if (!this.initialized) {
-                this.init();
-            }
-
-            return this.loadTemplate().then(template => {
-                return this.element.html(mustache.render(template, {settingsUpdate: this.stringify(this.settingsUpdate)}));
-            });
+    function stringify(input) {
+        try {
+            return JSON.stringify(input, null, 4);
+        } catch (err) {
+            return input;
         }
     }
-});
+
+})();
+
