df97fcc382328d31c12a1791437d5be53edec7bc
Adiel <adiela@wix.com>
2016-12-20 18:45:15 +0200

    adding compositions

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -3,29 +3,4 @@ require('./css/fontFace.scss')
 require('./css/animations.scss')
 require('./css/settings.scss')
 require('./css/support.scss')
-
-var Wix = require('Wix')
-var $ = require('jquery')
-
-
-function onUpdate(key, value) {
-  Wix.Settings.triggerSettingsUpdatedEvent({key: key, value: value});
-}
-
-function attachListeners() {
-  $('[wix-ctrl]').each(function (index, element) {
-    var $element = $(element);
-    var ctrl = $element.getCtrl();
-    if ($.isFunction(ctrl.onChange)) {
-      ctrl.onChange(function (value) {
-        onUpdate($element.attr('wix-param'), value);
-      })
-    }
-  });
-
-  $('#main-cta').getCtrl().onClick(function () {
-    console.log('This is your call-to-action, take it seriously');
-  })
-}
-
-$(attachListeners);
+require('./settings.scss')
